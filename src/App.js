1277614669ff6091c646d994a952ff6e7d2b5c67
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-12 23:09:06 +0530

    Initial Routing configuration added. Since doesn't have Swith enabled in this commit launching the application all of the links would be visible. so the next task would be to enable switch. closes #2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from "./Nav"
+import About from "./About"
+import Shop from "./Shop"
+import Home from "./Home"
 
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-      <h1>Home page</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/shop" component={Shop} />
+      </div>
+    </Router>
   );
 }
 
